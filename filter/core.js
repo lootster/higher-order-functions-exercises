e48ad9f84aa5ff6ac174b47320a6ad9678536f0e
lootster <sheldonkhlim@gmail.com>
2018-09-24 21:56:19 +0800

    more methods to solutions

--- filter/core.js
+++ filter/core.js
@@ -1,22 +1,57 @@
+// Method 1
+// function onlyEven(array) {
+//   return array.filter(number => number % 2 === 0);
+// }
+// Method 2
 function onlyEven(array) {
-  return array.filter(number => number % 2 === 0);
-}
-
-function onlySingleWord(array) {
-  return array.filter(word => !(word.includes(" ")));
-}
-
-function positiveRowsOnly(matrix) {
-  return matrix.filter(array => {
-    for (let i = 0; i < array.length; i++){
-      if (array[i] < 0) return false;
-    } 
-    return true;
+  return array.filter(function (number) {
+    return  number % 2 === 0;
   });
 }
 
+// Method 1
+// function onlySingleWord(array) {
+//   return array.filter(word => !(word.includes(" ")));
+// }
+// Method 2
+function onlySingleWord(array) {
+  return array.filter(function (word) {
+    return !(word.includes(" "))
+  });
+}
+
+// Method 1
+// function positiveRowsOnly(matrix) {
+//   return matrix.filter(array => {
+//     for (let i = 0; i < array.length; i++){
+//       if (array[i] < 0) return false;
+//     } 
+//     return true;
+//   });
+// }
+// Method 2
+// function positiveRowsOnly(matrix) {
+//   return matrix.filter(array => array.every(num => num > 0));
+// }
+// Method 3
+function positiveRowsOnly(matrix) {
+  return matrix.filter(function (array) {
+    return array.every(function (num) {
+      return num > 0;
+    });
+  });
+}
+
+// Method 1
+// function truthyValuesOnly(array) {
+//   return array.filter(element => element);
+// }
+
+// Method 2
 function truthyValuesOnly(array) {
-  return array.filter(element => element);
+  return array.filter(function (element) {
+    return element;
+  });
 }
 
 module.exports = {
